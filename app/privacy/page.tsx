0b9198fb2c0f387a871d65c460eefd9a5028329c
Jay <1598353326@qq.com>
2024-03-18 22:41:16 +0800

    chore: change privacy policy

--- app/privacy/page.tsx
+++ app/privacy/page.tsx
@@ -12,59 +12,73 @@ export default function TermsAndConditions() {
             <h1 className="mb-8 text-2xl font-bold md:text-4xl">
                 Privacy Policy
             </h1>
+            <p className="mb-4">
+                The Platform is a website dedicated to sharing the best design resources around the web.
+                These resources, including tools,
+                libraries, tutorials, starters, and other related content, are
+                shared on the Platform for informational purposes. Please note that
+                these resources are not owned by the Platform and are provided by
+                various contributors.
+            </p>
+            <p className="mb-4">
+                We value your privacy and are dedicated to protecting your personal data.
+                If you have any questions, comments, or concerns regarding this Privacy Policy,
+                our data practices, or would like to exercise your rights, do not hesitate to contact us.
+            </p>
             <ul className="text-sm md:text-base">
                 <h3 className="mb-4 text-xl font-medium">
-                    1. Resource Sharing
+                    1. Data Collection and Purpose
                 </h3>
                 <li>
-                    The Platform is a website dedicated to sharing the best design resources around the web.
-                    These resources, including tools,
-                    libraries, tutorials, starters, and other related content, are
-                    shared on the Platform for informational purposes. Please note that
-                    these resources are not owned by the Platform and are provided by
-                    various contributors.
+                    With your explicit consent and authorization, we collect only the user{`'`}s name and email address as necessary information
+                    to provide services or for identity verification. The username is required for
+                    submitting articles and comments, while the email address is used for notifying
+                    users about changes to the privacy policy or receiving user feedback.
                 </li>
                 <h3 className="my-4 text-xl font-medium">
-                    2. To Whom Does This Policy Apply
+                    2. Data Security
                 </h3>
                 <li>
-                    This Privacy Policy applies to customers and site visitors.
-                    Each customer is responsible for posting its own terms, conditions,
-                    and privacy policies, and ensuring compliance with all applicable laws and regulations.
+                    We employ industry-standard security measures, including data encryption and
+                    access controls, to protect your information from unauthorized access, disclosure,
+                    or damage. Additionally, we do not sell, rent, or share your personal information in any way.
                 </li>
                 <h3 className="my-4 text-xl font-medium">
-                    3. Changes To This Privacy Policy
+                    3. User Permissions
                 </h3>
                 <li>
-                    This Privacy Policy may change from time to time, as our Platform and our business may change.
-                    Your continued use of the Platform after any changes to
-                    this Privacy Policy indicates your agreement with the terms of the revised Privacy Policy.
+                    You have the right to revoke access to our application at any time. When using the Platform,
+                    You have the option to delete your account and data by accessing the{" "}
+                    <Link
+                        href="/user-settings"
+                        className={buttonVariants({ variant: "link", size: "link" })}
+                    >
+                        Account Settings
+                    </Link>{" "}
+                    section of the Platform.
                 </li>
                 <h3 className="my-4 text-xl font-medium">
-                    4. What Information Do We Collect
+                    4. Changes to This Privacy Policy
                 </h3>
                 <li>
-                    We collect information directly from you when you provide it to us explicitly on our Site.
-                    We do not use third-party cookies on our Site.
+                    We may update our privacy policy from time to time. Any changes
+                    will be posted on this page. If there are any significant changes in how we handle
+                    personal information, we will notify you promptly and seek your authorization.
                 </li>
                 <h3 className="my-4 text-xl font-medium">
-                    5. What We Use Your Information For
+                    5. How to Contact Us
                 </h3>
                 <li>
-                    We use your information to provide our Services, to improve our Platform,
-                    and to understand how you use our Platform.
+                    If you have any questions or concerns about our Privacy Policy, please contact us at{" "}
+                    <Link
+                        href={`mailto:${env.NEXT_PUBLIC_CONTACT_EMAIL}`}
+                        className={buttonVariants({ variant: "link", size: "link" })}
+                    >
+                        {env.NEXT_PUBLIC_CONTACT_EMAIL}
+                    </Link>
+                    .
                 </li>
             </ul>
-            <div className="my-8 text-left">
-                If you have any questions, please contact me at{" "}
-                <Link
-                    href={`mailto:${env.NEXT_PUBLIC_CONTACT_EMAIL}`}
-                    className={buttonVariants({ variant: "link", size: "link" })}
-                >
-                    {env.NEXT_PUBLIC_CONTACT_EMAIL}
-                </Link>
-                .
-            </div>
         </div>
     );
 };
